87554c192d33801ebffce7970828aabb9aef3616
rogermarcen <49266581+rogermarcen@users.noreply.github.com>
2022-07-11 17:25:27 +0200

    Delete musicConfig.sh

--- musicConfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo echo "silence" >> /var/www/cmdMusic
-sudo chmod 777 /var/www/cmdMusic
-sudo echo "0" > ./metaMusic
-sudo echo "1:OFF" >> ./metaMusic
-sudo echo "2:OFF" >> ./metaMusic
-sudo echo "3:OFF" >> ./metaMusic
